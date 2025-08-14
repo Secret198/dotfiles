df9d1f99e6c5f01c76ed19a4352bc95e1d0bbc9f
Secret198 <96205404+Secret198@users.noreply.github.com>
2025-08-14 15:23:16 +0200

    updated windows install script

--- Windows/Install.ps1
+++ Windows/Install.ps1
@@ -2,7 +2,13 @@
 # winget install -id 9N0DX20HK701 --silent --accept-package-agreements --accept-source-agreements #This is the terminal
 # winget install -id Starship.Starship --silent --accept-package-agreements --accept-source-agreements
 
-New-Item -itemtype symboliclink -path C:\Users\User\Documents\WindowsPowerShell -name Microsoft.PowerShell_profile.ps1 -value Microsoft.PowerShell_profile.ps1
-New-Item -itemtype symboliclink -path C:\Users\User -name .gitconfig -value .gitconfig
-New-Item -itemtype symboliclink -path C:\Users\User -name .vscode -value .vscode
-New-Item -itemtype symboliclink -path C:\Users\User -name .glzr -value .glzr
+$userName = "Yeah"
+
+$homePath = "C:\Users\"+$userName
+$powerShellPath = $homePath + "\Documents\WindowsPowerShell"
+
+New-Item -itemtype symboliclink -path $powerShellPath -name Microsoft.PowerShell_profile.ps1 -value Microsoft.PowerShell_profile.ps1
+
+New-Item -itemtype symboliclink -path $homePath -name .gitconfig -value .gitconfig
+New-Item -itemtype symboliclink -path $homePath -name .vscode -value .vscode
+New-Item -itemtype symboliclink -path $homePath -name .glzr -value .glzr
